9d630996b880bf38301942a8c30da9ecf93cfd34
bibekgupta07 <69662848+bibekgupta07@users.noreply.github.com>
2023-12-29 10:47:31 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 //"" =  false
@@ -22,5 +22,45 @@ console.log(booleanIsLoggedIn)
 let someNumber =33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************* operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " bibek"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
